4226a772c6fb5c0ceb9d8a7d61f6e3ed406262f9
Karel <kareltrisnanto26@gmail.com>
2023-04-26 12:01:34 +0700

    Update Fix Error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,25 @@
-import type { Config } from 'tailwindcss'
-const plugin = require('tailwindcss/plugin');
+import type { Config } from "tailwindcss";
+const plugin = require("tailwindcss/plugin");
 
 export default {
-  content: ["./src/**/*.{html,css,js,ts}"],
+  content: ["./src/**/*.{html,css,js,ts}", "./index.html"],
   darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         Poppins: ["Poppins"],
         NotoSans: ["Noto Sans"],
-        Volkhov: ["Volkhov"]
+        Volkhov: ["Volkhov"],
       },
       colors: {
         primaryColor: "#DF6951",
         secondaryColor: "#F1A501",
       },
       textShadow: {
-        sm: '1px 1px 1px rgba(0, 0, 0, .5)',
-        DEFAULT: '0 2px 4px rgba(0, 0, 0, .5)',
-        lg: '0 8px 16px rgba(0, 0, 0, .5)',
-        none: '0 0 0 rgba(0, 0, 0, 0)',
+        sm: "1px 1px 1px rgba(0, 0, 0, .5)",
+        DEFAULT: "0 2px 4px rgba(0, 0, 0, .5)",
+        lg: "0 8px 16px rgba(0, 0, 0, .5)",
+        none: "0 0 0 rgba(0, 0, 0, 0)",
       },
     },
   },
@@ -27,13 +27,12 @@ export default {
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
         {
-          'text-shadow': (value) => ({
+          "text-shadow": (value) => ({
             textShadow: value,
           }),
         },
-        { values: theme('textShadow') }
-      )
+        { values: theme("textShadow") }
+      );
     }),
   ],
-} satisfies Config
-
+} satisfies Config;
